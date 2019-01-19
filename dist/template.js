81c6c65315309d5f54eec316ee802b8c147e3095
Annie <annie@legenddigital.com.au>
2019-01-19 18:23:49 +0800

    data

--- dist/template.js
+++ dist/template.js
@@ -1,6 +1,8 @@
 var $history = {
     init: function () {
-        var datas = {}, data = [];
+        var datas = {};
+        var title = '历史记录';
+        var data = [];
         for(var i = 1; i < 15; i++){
             var dd = {};
             if(i % 3 === 0){
@@ -17,20 +19,21 @@ var $history = {
             dd.odds = i / 10;
             data.push(dd);
         }
+        datas.title = title;
         datas.data = data;
-
         //渲染数据
+        $history.renderIndexDom("title", "titletpl", datas);
         $history.renderIndexDom("history", "historytpl", datas);
     },
-
     //渲染模板
-    renderIndexDom:function(domid, tplid, data){
+    renderIndexDom: function (domid, tplid, data) {
         var html = template(tplid, data);
         document.getElementById(domid).innerHTML = html;
     }
+
 }
 
-
 $(function () {
+    //初始化数据
     $history.init();
 });
